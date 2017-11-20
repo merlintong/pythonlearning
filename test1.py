dc1a6973e4d0c875dc1af181da684cb7988e6f52
Merlin Tong <merlintong@email.com>
2017-11-20 11:14:30 -0500

    Test Commit

--- test1.py
+++ test1.py
@@ -1,1 +1,19 @@
-print("Hello World!")
+#purely testing
+word1="192.168.10.1"
+word2="GIE2PLABS010001"
+word3="10.100.17.11"
+result="test"
+result2=word2[-6:]
+result3=str(int(word2[-6:-3]))+"."+str(int(word2[-3:]))
+
+if word2[3]=="1":
+    result="10.100"
+elif word2[3]=="2":
+    result="10.104"
+elif word2[3]=="4":
+    result="10.107"
+elif word2[3]=="5":
+    result="10.108"
+
+result=result+"."+str(int(word2[-6:-3]))+"."+str(int(word2[-3:]))
+print (result)
